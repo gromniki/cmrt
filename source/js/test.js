eafe18fe8f8bb19bdc2f3e7c8d12141152dcdf38
Roman Golubev <gromniki@gmail.com>
2019-09-20 18:04:57 +0300

    Подготовка тега template, v2

---
+++ source/js/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const shopTemplate = document.querySelector('#shop').content.querySelector('.catalog__item');
+
+// const shopList = document.querySelector('.catalog__list');
+//
+// console.log(shopTemplate);
